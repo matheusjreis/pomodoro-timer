15190ef7d939f26c04a77dddf6efe9f4601ebbdb
Matheus Reis <matheusjosereis99@gmail.com>
2021-05-20 23:57:07 -0300

    Deleting useless images, and designing the watch shape

--- src/Script.js
+++ src/Script.js
@@ -6,34 +6,3 @@ Timer.showWatch({
 })
 
 Timer.startCounter()
-
-
-// let second = 1*1000
-// let minute = 60000
-// let interval = null
-
-// function start_counter(){
-//     interval = setInterval(notify(),3000)
-//     // console.log(interval)
-// }
-
-// function notify() {
-//     minute--
-//     console.log(minute)
-//     // new Notification("Primeira pausa", {
-//     //     body: "Hora de descansar"
-//     // })
-// }
-
-// function stopNotify(){
-//     clearInterval(interval)
-// }
-
-// function sleep(seconds) {
-//     let milisseconds = seconds*1000
-//     const date = Date.now()
-//     let currentDate
-//     do{
-//         currentDate = Date.now()
-//     }while(currentDate - date < milisseconds)
-// }
